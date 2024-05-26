ef97e746dd0e369e0c37aaa855368f38af0d530d
nataliia <sheludkonataliyavaleriivna@gmail.com>
2024-05-26 12:31:51 +0300

    add google fonts-links

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
+import App from "./components/App/App";
 import "./index.module.css";
 import { BrowserRouter } from "react-router-dom";
 import "modern-normalize";
